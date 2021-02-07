087b774f70541414c123b5ab3c25003f34962174
Caitlin Crews <caitlinrcrews@gmail.com>
2021-02-06 22:55:31 -0500

    set up container call

--- src/App.js
+++ src/App.js
@@ -1,57 +1,13 @@
 import React from "react";
 
-import Header from "./components/Header";
 import Container from "./components/Container"
-import Search from "./components/Search";
 
 const App = () => {
   return (
     <div>
-      <Header />
-      <Search />
-      <Container>
-      </Container>
+      <Container />
     </div>
   );
 };
-
-
-// function App() {
-//   const [state, setState] = useState(null)
-
-//   // useEffect(() => {
-//   //   async function getData () {
-//   //     const url = "https://randomuser.me/api/?results=250&nat=us";
-
-//   //     const users = await fetch(url).then(response => {
-//   //       return response
-//   //     });
-
-//   //     setState(users)
-//   //     console.log(users)
-//   //   }
-//   //   getData()m
-//   // }, []);
-
-//   useEffect(() => {
-//     API.getUsers().then((results) => {
-//       setState(results.data.results);
-//       //console.log(results.data.results);
-//     });
-//   }, []);
-
-//   console.log(state);
-//   return (
-//     <div className="App">
-//       <h1>Employees</h1>
-//       <ul>
-//         {state.map((user) => (
-//           <li>{`${user.name.first} ${user.name.last}`}</li>
-//         ))}
-
-//       </ul>
-//     </div>
-//   );
-// }
 
 export default App;
