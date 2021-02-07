e724f1672fe9f8def84b3f919e5baa0eebfa1e3e
Caitlin Crews <caitlinrcrews@gmail.com>
2021-02-06 23:22:03 -0500

    updated link to get us only users

--- src/utils/API.js
+++ src/utils/API.js
@@ -4,6 +4,6 @@ import axios from "axios";
 export default {
   // Gets all users
   search: function () {
-    return axios.get("https://randomuser.me/api/?results=100");
+    return axios.get("https://randomuser.me/api/?results=100&nat=us");
   },
 };
