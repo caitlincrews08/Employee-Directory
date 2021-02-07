7142390375d31877dd86ab4ddd8e6d403372d7b4
Caitlin Crews <caitlinrcrews@gmail.com>
2021-02-06 21:43:55 -0500

    added Search

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React from "react";
 
 import Header from "./components/Header";
 import Container from "./components/Container"
+import Search from "./components/Search";
 
 const App = () => {
   return (
     <div>
       <Header />
+      <Search />
       <Container>
-
       </Container>
     </div>
   );
